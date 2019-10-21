8e335e1943f9e4755e6b5e919aa87c1fa6f4009f
Peter Williams <pedroeldiablo@yahoo.com>
2019-10-21 15:24:28 +0100

    adding preview

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-d3.select("#new-note")
-    .on('submit', function() {
-      d3.event.preventDefault();
-      var input = d3.select('input');
-      d3.select("#notes")
-        .append('p')
-          .classed('note', true)
-          .text(input.property('value'));
-      input.property('value', '');
-    });
+var input = d3.select('input');
+var preview = d3.select('.preview');
+
+d3.select('#new-note')
+  .on('input', function() {
+    preview.classed('hide', false)
+      .text(input.property('value'));
+  })
+  .on('submit', function() {
+    d3.event.preventDefault();
+    d3.select('#notes')
+      .append('p')
+      .classed('note', true)
+      .text(input.property('value'));
+    input.property('value', '');
+    preview.classed('hide', true)
+      .text('');
+  });
